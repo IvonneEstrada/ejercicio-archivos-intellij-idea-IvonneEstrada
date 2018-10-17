13cf6f9bc6b24494fd6b5c157c9b51d3e5d1b957
IvonneEstrada <ivonne_montserrat@outlook.com>
2018-10-17 04:58:11 -0500

    Leyendo datos del archivo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hola mundo");
+	    //Ejercicio; Leer las calificaciones de los alumnos desde un archivo de texto y calcular su promedio
+        //El archivo contiene el nombre del alumno.
+        //Nombre de la materia
+        //La cantidad de calificaciones
+        //y sus valores.
+        File archivo = new File( "calificaciones.txt");
+        try {
+            Scanner entrada = new Scanner(archivo);
+            while (entrada.hasNext())   {
+                String nombreAlumno = entrada.next();
+                String nombreMateria = entrada.next();
+                int totalCalificaciones = entrada.nextInt();
+                entrada.next(); //omitir el salto de linea
+                //hacer un ciclo para leer el total de calificaciones
+                //...
+                //Calcular el promedio e imprimir
+            }
+        }
+        catch (FileNotFoundException exception) {
+            System.err.println("No se puede abrir el archivo");
+        }
+
     }
 }
